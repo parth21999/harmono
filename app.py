4733ff21ea628f7b547d26dd4353fbc50d449700
parthagg <parthagg@umich.edu>
2018-07-28 16:43:20 +0530

    fixed button spacing

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def hello_world():
 	return render_template('index.html')
 
-@app.after_request
-def after_request(response):
-	"""Disable caching"""
-	response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-	response.headers["Expires"] = 0
-	response.headers["Pragma"] = "no-cache"
-	return response
+# @app.after_request
+# def after_request(response):
+# 	"""Disable caching"""
+# 	response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+# 	response.headers["Expires"] = 0
+# 	response.headers["Pragma"] = "no-cache"
+# 	return response
